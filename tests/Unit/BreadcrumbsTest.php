41a7c9ddad1144606fdff10e9185f34c15ef6ded
godar <info@tropotek.com>
2026-02-27 15:47:20 +1100

    Remove Pest use Unit for Breadcrums testing

---
+++ tests/Unit/BreadcrumbsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BreadcrumbsTest extends TestCase
+{
+    public function test()
+    {
+        $crumbs = new \Tk\Breadcrumbs\Breadcrumbs();
+
+        $this->assertInstanceOf(\Tk\Breadcrumbs\Breadcrumbs::class, $crumbs);
+
+
+        // TODO Create a test case for crumbs
+
+
+
+
+
+
+
+    }
+
+
+}
