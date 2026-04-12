1740fda36d467d1dd00ce5812ea0e3ef5d4a7979
mickm <info@tropotek.com>
2026-04-12 12:56:31 +1000

    add livewire components to components folder with tkl-com namespace

--- packages/ttek/tkl-ui/src/TkBaseServiceProvider.php
+++ packages/ttek/tkl-ui/src/TkBaseServiceProvider.php
@@ -5,6 +5,7 @@ namespace Tk;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 use Tk\Support\Facades\Breadcrumbs;
 use Tk\Support\Facades\MenuBuilder;
 use Tk\View\Composers\DefaultPageName;
@@ -45,16 +46,24 @@ class TkBaseServiceProvider extends ServiceProvider
             __DIR__.'/../config/tkbase.php' => config_path('tkbase.php'),
         ], 'config');
 
+        // Livewire Volt component namespace
+        if (class_exists(\Livewire\Livewire::class)) {
+            Livewire::addNamespace(
+                namespace: 'tkl-pages',
+                viewPath: __DIR__.'/../Resources/views/pages',
+                classNamespace: 'Modules\\Staff\\Livewire',
+                classPath: __DIR__.'/../Livewire',
+                classViewPath: __DIR__.'/../Resources/views/pages',
+            );
+            Livewire::addNamespace('tkl-com', __DIR__.'/../resources/views/components');
+        }
+
         // Load views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'tkl-ui');
 
         // set a default controller TITLE if none set
         View::composer('*', DefaultPageName::class);
 
-        // Register Livewire components
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::addComponent('tkl-file-upload', viewPath: __DIR__ . '/../resources/views/livewire/⚡file-upload.blade.php');
-        }
 
         // Load routes (optional)
         // $this->loadRoutesFrom(__DIR__.'/routes/web.php');
